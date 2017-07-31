ecfa2fc07e95fa50f6f16374fcda9a018c6a61a5
Ryan L. Bell <ryan.l.bell@gmail.com>
2017-07-30 21:50:35 -0400

    Add fromNullable as a Maybe conversion

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,13 @@ import Just, { just } from './Just';
 import Maybe from './Maybe';
 import Nothing, { nothing } from './Nothing';
 
-export { Maybe, Just, just, Nothing, nothing };
+type Nullable = null | undefined;
+
+const fromNullable = <T>(v: T | Nullable): Maybe<T> => {
+  if (v) {
+    return just(v);
+  }
+  return nothing();
+};
+
+export { Maybe, Just, just, Nothing, nothing, Nullable, fromNullable };
