60fd5be5b71b8cbc8f5a4c5157249449ce44b710
Ryan L. Bell <ryan.l.bell@gmail.com>
2017-08-05 07:15:47 -0400

    Document fromNullable

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import Nothing, { nothing } from './Nothing';
 
 type Nullable = null | undefined;
 
+/**
+ * Converts a value that _may_ be null or undefined to a maybe.
+ * If the value is null or undefined, then Nothing is returned.
+ * If the value is anything else, it is returned wrapped in a Just.
+ */
 const fromNullable = <T>(v: T | Nullable): Maybe<T> => {
   if (v) {
     return just(v);
